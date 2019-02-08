516529d401d5b89ac895dbf45b6d9eb53a2c4827
Martyn Nevers <martyn.nevers@tfgm.com>
2019-02-08 10:46:33 +0000

    SC-953 json added for invalid token

--- routes/v1RequestDebtRecovery.js
+++ routes/v1RequestDebtRecovery.js
@@ -3,5 +3,35 @@
  */
 exports.post = function(req, res)
 {
+    res.set('Content-Type', 'application/json');
+
+    if (req.get('x-api-key') === undefined)
+    {
+        res.status(403);
+        return res.render('basic/v1Common/errors/invalidHeader');
+    }
+    else if (req.get('x-api-key') !== 'H7CAGfY1wV4Ofcz4hZMB04bhpYVJm8xiqJdEUtPi')
+    {
+        res.status(403);
+        return res.render('basic/v1Common/errors/invalidHeader');
+    }
     
+    var token = req.body['Token'];
+    if ((token === '') || (token === null))
+    {
+        res.status(400);
+        return res.send('NULL TOKEN SCENARIO NOT IMPLEMENTED');
+    }
+
+    switch (token)
+    {
+        case '':
+            return res.render('basic/v1RequestDebtRecovery/erros/invalidToken');
+
+        case 'XXX-1236-FFF':
+            return res.send('VALID TOKEN SCENARIO NOT IMPLEMENTED');
+
+        default:
+            return res.render('basic/v1RequestDebtRecovery/erros/invalidToken')
+    }
 }
